9c33a9c73154ad40d0362664cbaac42aa061b83f
nzbin <nzb329@163.com>
2022-03-25 19:39:53 +0800

    chore(examples): upgrade dependencies and small fix

--- tools/tasks/build.ts
+++ tools/tasks/build.ts
@@ -4,15 +4,18 @@ import { catchError } from "rxjs/operators";
 
 export const isWatchMode = !!process.env.WATCH_MODE;
 
+const a = require('../../tsconfig.build.json')
+
 ngPackagr()
   .forProject("./ng-package.js")
   .withTsConfig("../../tsconfig.build.json")
   .withOptions({
-    watch: isWatchMode
+    watch: isWatchMode,
   })
   .buildAsObservable()
   .pipe(
     catchError(() => {
+      console.log(a)
       if (!isWatchMode) {
         process.exitCode = 1;
       }
