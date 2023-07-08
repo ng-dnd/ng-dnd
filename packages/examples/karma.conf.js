36af47541fc65c6ad39990ac9b2cfad642cd45fc
nzbin <nzb329@163.com>
2023-07-08 16:24:59 +0800

    test: fix unit testing for karma

--- packages/examples/karma.conf.js
+++ packages/examples/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/starter-v11'),
+      dir: require('path').join(__dirname, './coverage/ng-dnd-examples'),
       subdir: '.',
       reporters: [
         { type: 'html' },
