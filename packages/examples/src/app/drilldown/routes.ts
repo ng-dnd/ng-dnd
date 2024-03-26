6fa89948ebd19a1a044ccf3d5b811d1f2921ce5c
nzbin <nzb329@163.com>
2024-03-26 23:02:08 +0800

    fix: fix no provider for drilldown example

--- packages/examples/src/app/drilldown/routes.ts
+++ packages/examples/src/app/drilldown/routes.ts
@@ -1,4 +1,7 @@
 import { Routes } from '@angular/router';
 import { ContainerComponent } from './container.component';
+import { TreeService } from './tree.service';
 
-export const routes: Routes = [{ path: '', component: ContainerComponent }];
+export const routes: Routes = [
+  { path: '', component: ContainerComponent, providers: [TreeService] },
+];
