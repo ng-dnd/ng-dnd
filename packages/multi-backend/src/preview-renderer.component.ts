e6c4e25c3690ffaa30a794c64e087c09f53b2554
nzbin <nzb329@163.com>
2024-04-22 23:35:08 +0800

    chore: remove compatible codes for Firefox

--- packages/multi-backend/src/preview-renderer.component.ts
+++ packages/multi-backend/src/preview-renderer.component.ts
@@ -10,14 +10,11 @@ import { map } from 'rxjs/operators';
  * essentially just renders whatever is placed between its tags, but
  * in a `position: fixed` container that is translated according to
  * the drag in progress and how far it has travelled.
- *
- * It currently has a workaround for some Firefox versions where the
- * whole thing wouldn't re-render unless you animated the border.
  */
 @Component({
   selector: 'dnd-preview-renderer',
   template: `
-    <div class="firefox-bug" [style]="style$ | async">
+    <div [style]="style$ | async">
       <ng-content></ng-content>
     </div>
   `,
@@ -32,20 +29,6 @@ import { map } from 'rxjs/operators';
         top: 0;
         width: 100%;
         height: 100%;
-      }
-      @keyframes animatedBorder {
-        from {
-          border-color: rgba(0, 0, 0, 0);
-        }
-        to {
-          border-color: rgba(0, 0, 0, 1);
-        }
-      }
-      .firefox-bug {
-        animation-name: animatedBorder;
-        animation-duration: 1s;
-        animation-iteration-count: infinite;
-        animation-timing-function: linear;
       }
     `,
   ],
